cff3b4b91e2f05664faf7c13113057a8ec0f9b08
Louis Heimel <louisheimel1@gmail.com>
2022-12-05 19:12:57 -0600

    add work

--- day3/index.js
+++ day3/index.js
@@ -4,35 +4,49 @@ const compose = (...fns) => fns.reduce((a, e) => x => a(e(x)))
 const log = x => console.log(x)
 const toString = x => x.toString()
 const splitLines = lines => lines.split('\n')
-const tail = n => xs => xs.slice(xs.length - n - 1, n)
 const id = x => x
 const juxt = (...fns) => x => fns.map(f => f(x))
 const slice = (x, y) => z => z.slice(x, y)
 const split = splitter => xs => xs.split(splitter)
 const map = fn => xs => xs.map(fn)
-const filter = fn => xs => xs.filter(fn)
 const reduce = reducer => seed => arr => {
   if (arr.length == 0) {
     return seed
   }
   return reduce(reducer)(reducer(seed, arr[0]))(arr.slice(1))
 }
-const readInput = () => readFileSync('./input')
 
-const overlaps = ([x, y]) => {
-  console.log([x, y])
-  x = x.map(x => parseInt(x, 10))
-  y = y.map(y => parseInt(y, 10))
-  const contains = (x1, x2) => {
-    return (x1[0] <= x2[0] && x1[1] >= x2[1])
-  }
-  return contains(x, y) || contains(y, x)
-}
-compose(log, 
-  reduce((a, e) => e ? a + 1 : a)(0),
-  filter(overlaps),
-  map(compose(map(split('-')), split(','))),
-  x => x.slice(0, -1),
+const parse = compose(  xs => xs.slice(0, -1),
   splitLines,
   toString,
-  readInput)()
+  () => readFileSync('./input'))
+
+const  rucksackCompartments = map(x => [x.slice(0, x.length / 2), x.slice(x.length / 2)])
+compose(
+  log,
+  reduce((a, e) => a + e)(0),
+  map(x => {
+    const temp = x.charCodeAt(0) - 'a'.charCodeAt(0) + 1
+    if (temp >= 0) return temp
+    return temp + 58;
+  }),
+  xs => xs.flatMap(id),
+  map(x => [...x]),
+  map(x => {
+    const s = new Set();
+    for (const c of x[0]) {
+      if (x[1].has(c)) {
+        s.add(c)
+      }
+    }
+    for (const c of x[1]) {
+      if (x[0].has(c)) {
+        s.add(c)
+      }
+    }
+    return s
+  }),
+  map(x => x.map(x => new Set(x.split('')))),
+  rucksackCompartments,
+  parse
+)()
--- day3/part2.js
+++ day3/part2.js
@@ -4,38 +4,68 @@ const compose = (...fns) => fns.reduce((a, e) => x => a(e(x)))
 const log = x => console.log(x)
 const toString = x => x.toString()
 const splitLines = lines => lines.split('\n')
-const tail = n => xs => xs.slice(xs.length - n - 1, n)
 const id = x => x
 const juxt = (...fns) => x => fns.map(f => f(x))
 const slice = (x, y) => z => z.slice(x, y)
 const split = splitter => xs => xs.split(splitter)
 const map = fn => xs => xs.map(fn)
-const filter = fn => xs => xs.filter(fn)
 const reduce = reducer => seed => arr => {
   if (arr.length == 0) {
     return seed
   }
   return reduce(reducer)(reducer(seed, arr[0]))(arr.slice(1))
 }
-const readInput = () => readFileSync('./input')
 
-const overlaps = ([x, y]) => {
-  console.log([x, y])
-  x = x.map(x => parseInt(x, 10))
-  y = y.map(y => parseInt(y, 10))
-  const contains = (x1, x2) => {
-    const case1 = x1[0] <= x2[0] && x2[0] <= x1[1] 
-    const case2 = x2[0] <= x1[0] && x1[0] <= x2[1] 
-    return case1 || case2
-    
-  }
-  return contains(x, y) || contains(y, x)
-}
-compose(log, 
-  reduce((a, e) => e ? a + 1 : a)(0),
-  filter(overlaps),
-  map(compose(map(split('-')), split(','))),
-  x => x.slice(0, -1),
+const parse = compose(  xs => xs.slice(0, -1),
   splitLines,
   toString,
-  readInput)()
+  () => readFileSync('./input'))
+
+const chunkByThree = xs => {
+  const chunks = []
+  let chunk = []
+  let i = 0
+  while (i < xs.length) {
+    if (i > 0 && i % 3 == 0) {
+      chunks.push(chunk)
+      chunk = []
+      chunk.push(xs[i]);
+    } else {
+      chunk.push(xs[i])
+    }
+    i++
+  }
+  chunks.push(chunk)
+  return chunks
+}
+const commonLetter = xs => {
+  const [a, b, c] = xs;
+  const aSet = new Set(a);
+  const bSet = new Set(b);
+  const cSet = new Set(c);
+  const d = [...a, ...b, ...c];
+  const result = new Set();
+  console.log(d, aSet, bSet, cSet)
+  for (let x of d) {
+    if (aSet.has(x) && bSet.has(x) && cSet.has(x)) {
+      result.add(x)
+    }
+  }
+  return Array.from(result)
+}
+const priorityValue = x => {
+    const temp = x.charCodeAt(0) - 'a'.charCodeAt(0) + 1
+    if (temp >= 0) return temp
+    return temp + 58;
+}
+
+compose(
+  log,
+  reduce((a, e) => a + e)(0),
+  map(priorityValue),
+  xs => xs.flatMap(id),
+  map(commonLetter),
+  map(x => x.map(x => x.split(''))),
+  chunkByThree,
+  parse
+)()
